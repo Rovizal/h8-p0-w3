94a05b78dad29603c4fefa8a81d1dcb09cd7f0dd
Rovizal <vizalji@gmail.com>
2018-02-08 20:10:41 +0700

    tugas array 2

---
+++ exercise3.js
@@ -0,0 +1,41 @@
+// Buatlah sebuah fungsi dengan nama dataHandling dengan sebuah parameter untuk menerima argumen. Argumen yang akan diterima adalah sebuah array yang berisi beberapa array sejumlah n. Contoh input dapat dilihat dibawah:
+
+// //contoh input
+
+
+function dataHandling() {
+ var input = [
+                ["0001", "Roman Alamsyah", "Bandar Lampung", "21/05/1989", "Membaca"],
+                ["0002", "Dika Sembiring", "Medan", "10/10/1992", "Bermain Gitar"],
+                ["0003", "Winona", "Ambon", "25/12/1965", "Memasak"],
+                ["0004", "Bintang Senjaya", "Martapura", "6/4/1970", "Berkebun"]
+            ];
+             for(var i = 0; i < input.length; i++){
+   console.log('Nomor ID: ' + input[i][0] + '\n' + 'Nama: ' + input[i][1] + '\n'
+                + 'TTL: ' + input[i][2] +' '+ input [i][3] +'\n' + 'Hobi: ' + input[i][4] + '\n' );
+  }
+}
+
+console.log (dataHandling());
+
+
+//OUTPUT
+// Nomor ID:  0001
+// Nama Lengkap:  Roman Alamsyah
+// TTL:  Bandar Lampung 21/05/1989
+// Hobi:  Membaca
+
+// Nomor ID:  0002
+// Nama Lengkap:  Dika Sembiring
+// TTL:  Medan 10/10/1992
+// Hobi:  Bermain Gitar
+
+// Nomor ID:  0003
+// Nama Lengkap:  Winona
+// TTL:  Ambon 25/12/1965
+// Hobi:  Memasak
+
+// Nomor ID:  0004
+// Nama Lengkap:  Bintang Senjaya
+// TTL:  Martapura 6/4/1970
+// Hobi:  Berkebun
